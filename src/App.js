01d80d0160ab5a5f4c7418acf5530dae11c84595
Abdallah <abdallah.yossf@gmail.com>
2022-04-05 01:53:34 +0200

    Test

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import { Footer } from './components/Footer';
 import { Nav } from './components/Nav';
@@ -6,21 +6,33 @@ import { Body } from './components/Body';
 import run from './initializer';
 
 function App() {
-  useEffect(() => {
-    run()
-    console.error("If you see any warning(s), it is because of this popular issue: https://www.youtube.com/watch?v=dQw4w9WgXcQ");
-  }, [])
-  return (
-    <div id="App">
-      <canvas id="bg"></canvas>
+    const [result, setResult] = useState("")
 
-      <div id="main">
-        <Nav/>
-        <Body/>
-        <Footer />
-      </div>
-    </div>
-  );
+    const handleClick = () => {
+        fetch('http://156.194.141.62:8000')
+        .then(response => response.text())
+        .then(data => setResult(result));
+    }
+
+    useEffect(() => {
+        run()
+        console.error("If you see any warning(s), it is because of this popular issue: https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+    }, [])
+    return (
+        <div id="App">
+            <canvas id="bg"></canvas>
+
+            <div id="main">
+                <Nav />
+                <Body />
+                <Footer />
+            </div>
+
+
+            <button onClick={handleClick}>Press me</button>
+            <div>{result}</div>
+        </div>
+    );
 }
 
 export default App;
