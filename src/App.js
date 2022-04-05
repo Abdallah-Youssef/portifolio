2d29648b6fc9d38f1f8e972b49a0267ac0a65808
Abdallah <abdallah.yossf@gmail.com>
2022-04-05 02:22:27 +0200

    Restored

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import './App.css';
 import { Footer } from './components/Footer';
 import { Nav } from './components/Nav';
@@ -6,31 +6,21 @@ import { Body } from './components/Body';
 import run from './initializer';
 
 function App() {
-    const [result, setResult] = useState("")
+  useEffect(() => {
+    run()
+    console.error("If you see any warning(s), it is because of this popular issue: https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+  }, [])
+  return (
+    <div id="App">
+      <canvas id="bg"></canvas>
 
-    const handleClick = () => {
-        fetch('http://156.194.141.62:8000')
-        .then(response => response.text())
-        .then(data => setResult(result));
-    }
-
-    useEffect(() => {
-        run()
-        console.error("If you see any warning(s), it is because of this popular issue: https://www.youtube.com/watch?v=dQw4w9WgXcQ");
-    }, [])
-    return (
-        <div id="App">
-            <canvas id="bg"></canvas>
-
-            <div id="main">
-                <Nav />
-                <Body />
-                <Footer />
-                <button onClick={handleClick}>Press me</button>
-                <div>{result}</div>
-            </div>
-        </div>
-    );
+      <div id="main">
+        <Nav/>
+        <Body/>
+        <Footer />
+      </div>
+    </div>
+  );
 }
 
 export default App;
